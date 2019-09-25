de57a7877a8208d07e62fba2af33c09643ca8fbe
Gary Her <gher3@ecc-jaguar.csuchico.edu>
2019-09-25 11:36:32 -0700

    Added replay option

--- main.cpp
+++ main.cpp
@@ -13,61 +13,52 @@ int main()
   char decision1 = ' ';
   char decision2 = ' ';
 
+  char play = 'y';
 
-  cout<<"Player 1, please enter your name: ";
-  cin>>player1name;
-  cout<<"Player 2, please enter your name: ";
-  cin>>player2name;
+  while(play == 'y'){
+  	cout<<"Player 1, please enter your name: ";
+  	cin>>player1name;
+  	cout<<"Player 2, please enter your name: ";
+  	cin>>player2name;
 
-  cout<<"WELCOME TO SPLIT OR STEAL!\n";
+  	cout<<"WELCOME TO SPLIT OR STEAL!\n";
 
-  cout<<player1name<<", please enter the character of your choice...\n";
-  cout<<"Please enter (s)plit or s(t)eal: ";
-  cin>>decision1;
+  	cout<<player1name<<", please enter the character of your choice...\n";
+  	cout<<"Please enter (s)plit or s(t)eal: ";
+  	cin>>decision1;
 
-  cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-      <<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-      <<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-      <<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+  	cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+      	<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+      	<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+      	<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 
-  cout<<player2name<<", please enter the character of your choice...\n";
-  cin>>decision2;
+  	cout<<player2name<<", please enter the character of your choice...\n";
+  	cin>>decision2;
 
-  if((decision1 == 't' || decision1 == 'T') && (decision2 == 's' || decision2 == 'S'))
-  {
-    cout<<player1name<<" stole while "<<player2name<<" split. "
-        <<player1name<<" WINS ALL OF THE PRIZE!\n";
+  	if((decision1 == 't' || decision1 == 'T') && (decision2 == 's' || decision2 == 'S'))
+  	{
+    	cout<<player1name<<" stole while "<<player2name<<" split. "
+        	<<player1name<<" WINS ALL OF THE PRIZE!\n";
+  	}
+  	else if((decision2 == 't' || decision2 == 'T') && (decision1 == 's' || decision1 == 'S'))
+  	{
+    	cout<<player2name<<" stole while "<<player1name<<" split. "
+        	<<player2name<<" WINS ALL OF THE PRIZE!\n";
+  	}
+  	else if((decision1 == 's' || decision1 == 'S') && (decision2 == 's' || decision2 == 'S'))
+  	{
+    	cout<<"Both players cooperated and chose to split. Congrats, "
+        	<<player1name<<" and "<<player2name<<" you each get half the prize!\n";
+  	}
+  	else
+  	{
+    	cout<<"Sorry, you were both greedy and chose to steal. Neither of you "
+        	<<"win a prize and go home with NOTHING!\n";
+  	}
+
+  	cout << "Do you want to play again? (y = yes, n = no)" << endl;
+	cin >> play;
   }
-  else if((decision2 == 't' || decision2 == 'T') && (decision1 == 's' || decision1 == 'S'))
-  {
-    cout<<player2name<<" stole while "<<player1name<<" split. "
-        <<player2name<<" WINS ALL OF THE PRIZE!\n";
-  }
-  else if((decision1 == 's' || decision1 == 'S') && (decision2 == 's' || decision2 == 'S'))
-  {
-    cout<<"Both players cooperated and chose to split. Congrats, "
-        <<player1name<<" and "<<player2name<<" you each get half the prize!\n";
-  }
-  else
-  {
-    cout<<"Sorry, you were both greedy and chose to steal. Neither of you "
-        <<"win a prize and go home with NOTHING!\n";
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   return 0;
 }
